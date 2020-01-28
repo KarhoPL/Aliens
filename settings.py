817c595b90648f8a7790a9aa6da96d4f84202757
Karolina <55796916+KarhoPL@users.noreply.github.com>
2020-01-28 19:36:53 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,3 +5,9 @@ class Settings():
         self.bg_color = (230,230,230)
 
         self.ship_speed_factor = 1.5
+
+        #ustawienia pocisku
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = 60, 60, 60 
