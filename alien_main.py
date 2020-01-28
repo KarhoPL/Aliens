f2061a08b3ec5e40d6a5fb4d67605d1367b966d8
Karolina <55796916+KarhoPL@users.noreply.github.com>
2020-01-28 19:36:29 +0100

    Update alien_main.py

--- alien_main.py
+++ alien_main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -12,12 +12,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Attack of Aliens")
     ship = Ship(ai_settings, screen)
+    bullets = Group()
     # rozpoczęcie głównej pętli gry
     while True:
         #ta pętla będzie działać po każdym "wydarzeniu" czyli kliknięciu klawisza lub myszy
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+        gf.update_screen(ai_settings,screen,ship, bullets)
 
 
 run_game()
