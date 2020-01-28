2f230117a19f319b9a4c3cf8c6ea4762096dbfad
Karolina <55796916+KarhoPL@users.noreply.github.com>
2020-01-28 19:35:58 +0100

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,11 +1,15 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def check_keydown_events(ai_settings, screen, event, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     if event.key == pygame.K_LEFT:
         ship.moving_left = True   
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
     
 def check_keyup_events(event,ship):
     if event.key == pygame.K_RIGHT:
@@ -14,17 +18,21 @@ def check_keyup_events(event,ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ship)
+            check_keydown_events(ai_settings,screen, event,ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-def update_screen(ai_settings,screen, ship):
+def update_screen(ai_settings,screen, ship, bullets):
     #odświeżenie ekranu po każdej iteracji pętli
     screen.fill(ai_settings.bg_color)
+
+    for bullet in bullets.sprites():
+        bullet.draw_bullets()
+
     ship.blitme()
     pygame.display.flip()
