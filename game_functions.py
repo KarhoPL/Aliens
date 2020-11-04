87336b037eb448180a8fc9a099aafe93aaffb9b4
Karolina Haracz <k.m.haracz@gmail.com>
2020-11-04 23:00:07 +0100

    now bullets kills aliens

--- game_functions.py
+++ game_functions.py
@@ -44,12 +44,13 @@ def update_screen(ai_settings,screen, ship, aliens, bullets):
     aliens.draw(screen)
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     bullets.update()
     # deleting bullets that are out of screen
     for bullet in bullets.copy():
         if bullet.rect.bottom <0 :
             bullets.remove(bullet)
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 def get_number_aliens_x(ai_settings,alien_width):
     avaible_space_x = ai_settings.screen_width - 2* alien_width
